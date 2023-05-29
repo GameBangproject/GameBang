04d7380dabeccfee93beb4801193cd26c862f90e
JunYoungKr <kop981020@naver.com>
2023-05-29 23:23:03 +0900

    [add]modal 추가

--- gamebang/src/components/Body/Body.js
+++ gamebang/src/components/Body/Body.js
@@ -4,11 +4,7 @@ import { Link, Navigate } from "react-router-dom";
 import "./Body.css";
 
 const Body = () => {
-  return (
-    <div className="bodyContainer">
-      <span style={{ color: "black" }}>This is Body</span>
-    </div>
-  );
+  return <div className="bodyContainer"></div>;
 };
 
 export default Body;
