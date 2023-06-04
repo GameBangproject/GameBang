fd31cd8860401aec62a7a80a422a46536ed792b9
yi-barrack <yby010525@naver.com>
2023-06-04 15:00:21 +0900

    배경화면 변경 및 블록게임 수정

--- gamebang/src/components/Header/Header.js
+++ gamebang/src/components/Header/Header.js
@@ -1,16 +1,14 @@
 import React from "react";
-
+import { Helmet } from 'react-helmet';
 import "./Header.css";
+
 const Header = () => {
   return (
     <div className="headerContainer">
-      <span
-        style={{
-          color: "white",
-          fontFamily: "Comic Sans MS",
-          fontSize: "100px",
-        }}
-      >
+      <Helmet>
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500&display=swap" rel="stylesheet" />
+      </Helmet>
+      <span className="gameTitle">
         GameBang
       </span>
     </div>
