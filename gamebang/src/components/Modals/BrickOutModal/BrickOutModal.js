ff8afe3a956930c48ad1dbfaaa1f9f75205e7ea3
JunYoungKr <kop981020@naver.com>
2023-06-04 15:44:24 +0900

    [add]최종 코드

--- gamebang/src/components/Modals/BrickOutModal/BrickOutModal.js
+++ gamebang/src/components/Modals/BrickOutModal/BrickOutModal.js
@@ -24,23 +24,24 @@ const BrickOutModal = ({ setModalOpen }) => {
     <div className="modal" ref={modalRef}>
       <div className="modal-content">
         <div className="game-demo">
-          <video src={BrickGameVideo} controls></video>
+          <video src={BrickGameVideo} autoPlay controls></video>
         </div>
         <div className="game-description-play">
           <div className="game-description">
             <h1 style={{ marginTop: "0px", marginBottom: "0px" }}>Brickout</h1>
             <hr></hr>
             <span style={{ fontSize: "20px" }}>
-              <strong>Brickout 게임은</strong> 플레이어가 패들을
-              좌우로 움직여 공을 튕겨내어 화면 상단의 벽돌들을 파괴해야 합니다.{" "}
-              <br />
+              <strong>Brickout 게임은</strong> 플레이어가 패들을 좌우로 움직여
+              공을 튕겨내어 화면 상단의 벽돌들을 파괴해야 합니다. <br />
               <br />
               공은 패들에 튕겨져 다시 위쪽으로 향하며, 벽돌을 맞추면 벽돌이
               사라집니다. 목표는 모든 벽돌을 제거하는 것입니다. <br />
               <br />
-              플레이어가 공을 튕겨내지 못하고 화면 아래로 떨어뜨리면, 게임이 종료됩니다.<br />
+              플레이어가 공을 튕겨내지 못하고 화면 아래로 떨어뜨리면, 게임이
+              종료됩니다.
               <br />
-              이 게임은 직관적이며 쉽게 플레이 가능하고, 공의 튕기는 방향이 변화하기 때문에 플레이어의 반응속도와 능력을 테스트합니다.
+              <br />이 게임은 직관적이며 쉽게 플레이 가능하고, 공의 튕기는
+              방향이 변화하기 때문에 플레이어의 반응속도와 능력을 테스트합니다.
             </span>
           </div>
           <a
